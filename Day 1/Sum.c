78e9655b35ddaf5ae6e2535a76bf702d32b145e7
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-01-22 10:53:24 +0530

    Add .gitignore and initial Sum.c program for calculating the sum of two numbers

---
+++ Day 1/Sum.c
@@ -0,0 +1,12 @@
+//! wap a c program  to display Sum of two numbers.
+
+#include <stdio.h>
+int main()
+{
+    int a, b, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("Sum of %d and %d is %d\n", a, b, sum);
+    return 0;
+}
