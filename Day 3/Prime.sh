961ae2cde056e531c84f022e496ada885c257da4
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-02-05 10:03:14 +0530

    Refactor Prime.sh to simplify prime number check logic and improve readability

--- Day 3/Prime.sh
+++ Day 3/Prime.sh
@@ -8,18 +8,13 @@ if (( num < 2 )); then
     exit 0
 fi
 
-is_prime=1
 for (( i=2; i*i<=num; i++ )); do
     if (( num%i == 0 )); then
-        is_prime=0
-        break
+        echo "$num is not a prime number."
+        exit 0
     fi
-done 
-if (( is_prime )); then
-    echo "$num is a prime number."
-else
-    echo "$num is not a prime number."
-fi
+done
+echo "$num is a prime number."
 
 
 #?    How to compile and run the code
