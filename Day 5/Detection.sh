4d18f1cf6a41d77ff8dec54b35b6a999841ce196
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-03-19 10:51:02 +0530

    Add usage instructions and example output to Deadlock Detection Algorithm script

--- Day 5/Detection.sh
+++ Day 5/Detection.sh
@@ -76,3 +76,17 @@ else
     done
     echo
 fi
+
+
+
+
+# ?    How to compile and run the code
+# *    1. Open your terminal.
+# *    2. Navigate to the directory where the Detection.sh file is located.
+# *    3. Make sure the script has execute permissions. You can set the permissions
+# *       using the command:
+# *       chmod +x "Day 5/Detection.sh"
+# *    4. Run the script using the command:
+# *       "Day 5/Detection.sh"
+# *    5. The output will display the scheduling results, including the average waiting time and average turnaround time for the given processes based on their burst times and arrival times.
+# ?    Example Output
