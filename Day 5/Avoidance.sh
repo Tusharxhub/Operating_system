686cb9b69a1c4f46412614d2c22cb519c578ed65
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-03-19 10:34:51 +0530

    Add Deadlock Avoidance Algorithm implementation script

---
+++ Day 5/Avoidance.sh
@@ -0,0 +1,8 @@
+#! Q1. Deadlock Avoidance Algorithm Implementation
+
+#! process      Allocation       Max           Avg
+#!               A    B       A    B         A   B
+#! P0            1    0       2    1         1   1
+#! P1            0    1       1    2         
+#! P2            1    1       2    2 
+
