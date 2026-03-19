c4fe2600470ccb09ca4b10f093935844625e2cf3
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-03-19 10:45:42 +0530

    Add Deadlock Detection Algorithm implementation script

---
+++ Day 5/Detection.sh
@@ -0,0 +1,13 @@
+# !  Deadlock Detection Algorithm Implementation
+
+process       Allocation          Request         Avg 
+            A     B               A     B        A   B
+P0          1     0               0     1        1   0
+P1          2     1              1     0
+P2          0     1              1     0
+P3          1     0              0     1
+
+
+
+
+
