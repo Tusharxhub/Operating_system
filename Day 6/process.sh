99521c7931ecb422ec452ac202b0c6caa9bb215b
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-04-01 20:14:45 +0530

    Refactor process script to improve readability and structure

--- Day 6/process.sh
+++ Day 6/process.sh
@@ -1,1 +1,17 @@
-# !  Create a child process and show the process ID of the parent and the child both
+# !  Create a child process and show the process ID of the parent and the child both.
+
+
+
+
+
+echo "Parent process ID: $$"
+
+( 
+	echo "Child process ID: $$"
+	sleep 1
+) &
+
+wait
+
+
+
