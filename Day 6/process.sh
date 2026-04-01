1445339f769e994010088fad6d65f8084cd96a1e
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-04-01 20:20:18 +0530

    Add usage instructions and example output to process script documentation

--- Day 6/process.sh
+++ Day 6/process.sh
@@ -14,4 +14,16 @@ echo "Parent process ID: $$"
 wait
 
 
+# ?    How to compile and run the code
+# *    1. Open your terminal.
+# *    2. Navigate to the directory where the process.sh file is located.
+# *    3. Make sure the script has execute permissions. You can set the permissions
+# *       using the command:
+# *       chmod +x "Day 6/process.sh"
+# *    4. Run the script using the command:
+# *       "Day 6/process.sh"
+# *    5. The output will display the scheduling results, including the average waiting time and average turnaround time for the given processes based on their burst times and arrival times.
+# ?    Example Output
+#* Parent process ID: 18408
+#* Child process ID: 18408
 
