ea2b3696e295e0c59cdc206fdb34711f9d8d7003
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2026-04-09 09:57:56 +0530

    Add initial script for calculating internal and external fragmentation using First Fit and Best Fit algorithms

---
+++ Day 7/1.sh
@@ -0,0 +1,10 @@
+
+
+#! Write a program to find the internal and external fragmentation
+#! in the following problem using:
+
+#! 1. First Fit Algorithm
+#! 2. Best Fit Algorithm
+
+#! Memory Blocks: 20KB, 10KB, 15KB, 12KB, 6KB
+#! Jobs: 5KB, 18KB, 12KB, 14KB
